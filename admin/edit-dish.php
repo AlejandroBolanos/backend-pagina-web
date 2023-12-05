e1a5fcaa05e2c2cd618da87a3022bc23b79499b9
AlejandroRB <alejandrorb0803@gmail.com>
2023-12-05 11:57:47 -0600

    add css at admin

--- admin/edit-dish.php
+++ admin/edit-dish.php
@@ -84,11 +84,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Destination</title>
-   <!-- <link rel="stylesheet" href="../css/themes/admin.css">-->
+    <link rel="stylesheet" href="../css/main.css" />
 </head>
 <body>
-    <div class="container">
-        <h2>Edit Destination</h2>
+    <div class="cont-edit">
+    <h2 class="title-admin">Edit Dishe</h2>
         <?php 
             echo $message;
         ?>
